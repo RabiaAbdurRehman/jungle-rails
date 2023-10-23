20529c70a8030e6ee22348412c1b6a8cf700806e
Rabia Abdur Rehman <rabia.abdur.rehman@gmail.com>
2023-10-23 05:17:53 -0600

    Add products count and categories count and link to dashboard.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,10 @@
 class Admin::DashboardController < ApplicationController
   def show
+    #Display a count of how many products are in the database
+    @product_count = Product.count
+    @products = Product.all
+    #Display a count of how many categories are in the database
+    @product_categories = Category.count
+    @categories = Category.all
   end
 end
